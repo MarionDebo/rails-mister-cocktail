ba3c4d501a0b68abec1bf78480deada5cb205fa5
Marion Debonnaire <marion.debonnaire@gmail.com>
2020-08-13 15:43:08 +0200

    Exo 2 - finsihing Cocktails Controller

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,9 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  # validates :description, uniqueness: { scope: [:ingredient_id, :cocktail_id] }
-  # validates :description, :uniqueness => { :scope => :ingredient_id, :cocktail_id }
-  # validates_uniqueness_of :description, scope: :ingredient_id, :cocktail_id
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
-
-
