514f692cb03737c16e457b1d760c724d7f55b5fd
Marion Debonnaire <marion.debonnaire@gmail.com>
2020-08-14 16:09:38 +0200

    Update

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,7 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
   # When you delete a cocktail, you should delete associated doses (but not the ingredients as they can be linked to other cocktails).
   validates :name, presence: true, uniqueness: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,7 @@
 class Ingredient < ApplicationRecord
   # belongs_to :cocktail
   has_many :doses
+  has_one_attached :photo
   # You can’t delete an ingredient if it is used by at least one cocktail.
 
   validates :name, presence: true, uniqueness: true
